81e35f5dadcb228dbeb7b00bb4e1e868268a5fe8
tonycode2 <velurn123@gmail.com>
2025-10-19 09:51:53 -0600

    Adding project

---
+++ user-auth/src/main/java/com/portfolio/tony/user/auth/user_auth/UserAuthApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.tony.user.auth.user_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthApplication.class, args);
+	}
+
+}
